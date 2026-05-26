db7afefcdfd860227e93fd497da1ac54a59afdd7
Damir <damir.sadykov0407@gmail.com>
2026-05-26 21:33:13 +0300

    feat: resources spawn on map

--- Assets/Scripts/Game/Foraging/ResourceGenerator.cs
+++ Assets/Scripts/Game/Foraging/ResourceGenerator.cs
@@ -2,15 +2,43 @@ using UnityEngine;
 
 public class ResourceGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Resource generation")]
+    public int maxResourcesOnMap = 100;
+    public int initialResourcesOnMap = 10;
+    public float spawnRate = 3.0f;
+    public GameObject[] resources;
+    public float generationBoundsMinX;
+    public float generationBoundsMaxX;
+    public float generationBoundsMinY;
+    public float generationBoundsMaxY;
+
+    private float timer = 0f;
+    private int resourceCounter;
+
     void Start()
     {
-        
+        for (int i = 0; i < initialResourcesOnMap; i++)
+        {
+            spawnResource();
+        }
+        resourceCounter = initialResourcesOnMap;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (resourceCounter >= maxResourcesOnMap) return;
+
+        timer += Time.deltaTime;
+        if (timer >= spawnRate)
+        {
+            timer = 0f;
+            spawnResource();
+        }
+    }
+
+    void spawnResource()
+    {
+        GameObject spawned = Instantiate(resources[Random.Range(0, resources.Length)], new Vector3(Random.Range(generationBoundsMinX, generationBoundsMaxX), Random.Range(generationBoundsMinY, generationBoundsMaxY), 0), Quaternion.identity);
+        spawned.transform.parent = this.transform;
     }
 }
