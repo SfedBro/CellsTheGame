c7a92e0bebd48a2ec686156549a4867fb7bf71b1
SfedBro <fegalinka@mail.ru>
2026-07-17 23:01:41 +0300

    Auto-equip weapon module feature when entering Foraging scene without weapon

--- Assets/Scripts/Game/Factory/Player/PlayerInteractor.cs
+++ Assets/Scripts/Game/Factory/Player/PlayerInteractor.cs
@@ -32,6 +32,10 @@ public class PlayerInteractor : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.X) && !Input.GetKey(KeyCode.LeftControl) && !Input.GetKey(KeyCode.RightControl))
         {
+            if (PlayerModuleManager.Instance != null)
+            {
+                PlayerModuleManager.Instance.EnsureWeaponEquipped();
+            }
             if (AutoSaveManager.Instance != null)
             {
                 AutoSaveManager.Instance.SaveAll();
